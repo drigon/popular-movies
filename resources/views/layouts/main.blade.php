767b24b006a710ca0dbfefd7bfbcd917514b54b6
drigon <drigon4ever@gmail.com>
2020-11-09 11:42:47 +0900

    Search Livewire

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -40,12 +40,7 @@
                     </li>
                 </ul>
                 <div class="flex flex-col md:flex-row items-center">
-                    <div class="relative mt-3 md:mt-0">
-                        <input type="text" class="bg-gray-800 rounded-full text-sm w-64 px-4 pl-9 py-1 focus:outline-none focus:shadow-outline" placeholder="Search">
-                        <div class="absolute top-0">
-                            <svg class="w-5 text-gray-500 mt-1 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                        </div>
-                    </div>
+                    <livewire:search-dropdown>
                     <div class="md:ml-4 mt-3 md:mt-0">
                         <a href="#">
                             <img src="/img/avatar.jpg" alt="avatar" class="rounded-full w-8 h-8">
